2d3bffd6ba824bd22f3219b9cfd04368f43da756
JeanCarlogomez <gomezortizjeancarlo14@gmail.com>
2024-09-28 20:53:48 -0500

    Creacion clase proveedor.py

---
+++ app/proveedor.py
@@ -0,0 +1,20 @@
+from producto import Producto
+
+class Proveedor:
+    def __init__(self, nombre: str, direccion: str, telefono: str):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.telefono = telefono
+        self.lista_productos = []
+    
+    def agregar_producto(self, producto):
+        self.lista_productos.append(producto)
+    
+    def eliminar_producto(self, producto):
+        if producto in self.lista_productos:
+            self.lista_productos.remove(producto)
+        else:
+            raise ValueError(f"Producto {producto.nombre} no encontrado en la lista del proveedor")
+    
+    def listar_productos(self):
+        return [producto.nombre for producto in self.lista_productos]
